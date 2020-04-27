cffef999ef50b1b74ebec7fccc4a2f3e72568e60
sankemax <sankeman2004@gmail.com>
2020-04-27 21:06:37 +0300

    wip before big change

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import Body from "./components/Body";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Config } from "./config/ConfigType";
+import { Container } from "@material-ui/core";
 import "./App.css";
 
 function App({ config }: { config: Config }) {
-  return (<div>
-    <Header />
-    <Body config={config} />
-    <Footer />
-  </div>
+  return (
+    <Container maxWidth="lg">
+      <Header />
+      <Body config={config} />
+      <Footer />
+    </Container>
   );
 }
 
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
 import { Item } from './ItemType';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            '& > *': {
+                margin: theme.spacing(1),
+            },
+        },
+    }),
+);
 
 export function ItemComponent({
     id,
@@ -8,12 +22,12 @@ export function ItemComponent({
     link,
     feedUrl,
     pubdate,
-    date,
     comments,
 }: Item) {
+    const classes = useStyles();
     return (
         <div>
-            
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         </div>
     )
 }
