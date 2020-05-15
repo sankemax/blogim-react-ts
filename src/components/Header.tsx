69b9c7b5e92486f99122ac8315ab5d56d1465dc3
sankemax <sankeman2004@gmail.com>
2020-05-15 03:37:15 +0300

    get blogim logo via https instead of http

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import "./Header.css";
 export default function Header() {
     return (
         <div className="AppBar" >
-            <img className="TitleFav" src="http://info.org.il/club/blogim-logo-small.png" alt="glasses illustration" />
+            <img className="TitleFav" src="https://info.org.il/club/blogim-logo-small.png" alt="glasses illustration" />
             <div className="Title">
                 כתיבה עצמאית מעניינת בעברית
             </div>
